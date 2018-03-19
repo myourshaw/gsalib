ae73c6375acae403b4ded036457437516a012bac
Michael Yourshaw <myourshaw@gmail.com>
2018-03-19 17:56:31 -0600

    correct typo in description

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='Python version of R gsalib R to read GATK Report tables into pandas DataFrames',  # Required
+    description='Python version of R gsalib to read GATK Report tables into pandas DataFrames',  # Required
 
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
